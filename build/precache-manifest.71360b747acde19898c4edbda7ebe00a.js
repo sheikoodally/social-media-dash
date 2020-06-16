39b88ae28eca2f40c6649ad9724c59dee9791b2c
sheikoodally <44279883+sheikoodally@users.noreply.github.com>
2020-06-16 13:33:12 -0400

    adding build

---
+++ build/precache-manifest.71360b747acde19898c4edbda7ebe00a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6dc29e2e55626b4ab8c6837f8666427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab05d937de674ae874b3",
+    "url": "/static/css/main.ce0f638e.chunk.css"
+  },
+  {
+    "revision": "8644cce229b3f529a173",
+    "url": "/static/js/2.ae244c43.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ae244c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab05d937de674ae874b3",
+    "url": "/static/js/main.ad1d44b6.chunk.js"
+  },
+  {
+    "revision": "287759eb27912f4b75fd",
+    "url": "/static/js/runtime-main.92d0e96c.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/static/media/icon-youtube.8499da12.svg"
+  }
+]);
